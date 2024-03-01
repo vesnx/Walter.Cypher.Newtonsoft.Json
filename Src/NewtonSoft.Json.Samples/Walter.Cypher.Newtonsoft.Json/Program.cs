db0821639745d1c32033df63ab5ec5d7b9289d75
VESNX SA <130899007+vesnx@users.noreply.github.com>
2024-03-01 13:02:27 +0100

    update sample
    add sample as well ad documentation

---
+++ Src/NewtonSoft.Json.Samples/Walter.Cypher.Newtonsoft.Json/Program.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+
+using System.Net;
+
+using Walter;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+namespace ProfileApp;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        //secure the json using a protected password
+        using var service = new ServiceCollection()
+                                .AddLogging(option =>
+                                {
+                                    //configure your logging as you would normally do
+                                    option.AddConsole();
+                                    option.SetMinimumLevel(LogLevel.Information);
+                                })
+                                .UseSymmetricEncryption("May$ecr!tPa$$w0rd")
+                                .BuildServiceProvider();
+
+        service.AddLoggingForWalter();//enable logging for the Walter framework classes
+
+
+        JsonSerializerSettings options = new JsonSerializerSettings()
+        {
+            ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
+            Formatting = Formatting.Indented,
+        };
+
+
+
+        //... rest of your code 
+
+        /*
+        save to json and store or send to a insecure location the profile to disk. 
+        Data in transit can be read even if TLS secured using proxy or man in the middle. 
+
+         */
+        var profile = new UserProfile()
+        {
+            Email = "My@email.com",
+            Name = "Jo Coder",
+            DateOfBirth = new DateTime(2001, 7, 16),
+            Devices = [IPAddress.Parse("192.168.1.1"), IPAddress.Parse("192.168.1.14"), IPAddress.Loopback]
+        };
+
+        var json = JsonConvert.SerializeObject(profile, options);
+        var fileName = Path.GetTempFileName();
+
+        //use inversion of control and generate a ILogger without dependency injection
+        // the logging framework will write json to the console so we can inspect it:-)
+        Inverse.GetLogger("MyConsoleApp")?.LogInformation("Cyphered json:\n{json}", json);
+
+
+        //save it to disk
+        await File.WriteAllTextAsync(fileName, json).ConfigureAwait(false);
+
+        //... rest of your code 
+
+
+        /*
+         Read the json back in to a class using this simple extension method
+         */
+        var cypheredJson = await File.ReadAllTextAsync(fileName).ConfigureAwait(false);
+
+
+
+        
+        //use string extension method from Walter NuGet package to generate json from a string
+        //this allows you to use generics to bind valid json to a output wich is not null
+        //if true
+        if (cypheredJson.IsValidJson<UserProfile>(options, out UserProfile? user))
+        {
+            //... user is not null and holds decrypted values as the console will show
+            Inverse.GetLogger("MyConsoleApp")?.LogInformation("Profile:\n{profile}", user.ToString());
+        }
+
+        //cleanup and delete the temp file
+        if (File.Exists(fileName))
+        {
+
+            File.Delete(fileName);
+        }
+
+    }
+}
+
---
+++ Src/NewtonSoft.Json.Samples/Walter.Cypher.Newtonsoft.Json/UserProfile.cs
@@ -0,0 +1,40 @@
+﻿// ***********************************************************************
+// Assembly         : ProfileApp
+// Author           : Walter Verhoeven
+// Created          : Fri 01-Mar-2024
+//
+// Last Modified By : Walter Verhoeven
+// Last Modified On : Fri 01-Mar-2024
+// ***********************************************************************
+// <copyright file="UserProfile.cs" company="VESNX SA">
+//     Copyright (c) VESNX SA. All rights reserved.
+// </copyright>
+// <summary>
+// show how to configure a class using annotation to obfuscate the properties
+// as well as it's content to be compliant with GDPR and privacy laws
+// </summary>
+// ***********************************************************************
+using Newtonsoft.Json;
+
+using System.Net;
+namespace ProfileApp;
+
+internal record UserProfile
+{
+    [JsonProperty("a")]
+    [JsonConverter(typeof(GDPRObfuscatedStringConverter))]
+    public required string Name { get; set; }
+
+    [JsonProperty("b")]
+    [JsonConverter(typeof(GDPRObfuscatedStringConverter))]
+    public required string Email { get; set; }
+
+    [JsonProperty("c")]
+    [JsonConverter(typeof(GDPRObfuscatedDateTimeConverter))]
+    public DateTime DateOfBirth { get; set; }
+    
+    [JsonProperty("d")]
+    [JsonConverter(typeof(GDPRIPAddressListConverter))]
+    public List<IPAddress> Devices { get; set; } = [];
+}
+
